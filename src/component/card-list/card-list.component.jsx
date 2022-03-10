5e3e65f80d96175a6a17a12e90d8dcd87eb652b4
Dustine Gutierrez <dustine.gutierrez@whitecloak.com>
2022-03-10 12:44:06 +0800

    add card

--- src/component/card-list/card-list.component.jsx
+++ src/component/card-list/card-list.component.jsx
@@ -1,5 +1,9 @@
 import './card-list.styles.css'
+import { Card } from '../card/card.component'
 
 export const CardList = (props) =>{
-    return <div className = 'card-list' >{props.children}</div>
+    return <div className = 'card-list' >      
+        {props.monsters.map(monsters => (
+        <Card key={monsters.id}  monsters={monsters}/>
+      ))} </div>
 }
---
+++ src/component/card/card.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Card = (props) => (
+    <div>
+        <h1>{props.monsters.name}</h1>
+    </div>
+)
