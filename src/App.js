aced624109bc90bb548fd6bc62fb939e68176a70
Dustine Gutierrez <dustine.gutierrez@whitecloak.com>
2022-03-10 11:29:11 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { CardList } from './component/card-list/card-list.component';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      monsters: []
+    }
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters:users}));
+  }
+
+  render(){
+    return(
+      <div className="App">
+      <CardList name="Dustine"/>
+        {this.state.monsters.map(monsters => (
+          <h1 key={monsters.id}> 
+            {monsters.name}<br></br>
+            {monsters.username} 
+          </h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
