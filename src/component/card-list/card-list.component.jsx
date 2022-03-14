1a4294f6af800e8a0f35f5f245af1185726a3f6a
Dustine Gutierrez <dustine.gutierrez@whitecloak.com>
2022-03-14 14:21:02 +0800

    Updates

--- src/component/card-list/card-list.component.jsx
+++
@@ -1,9 +0,0 @@
-import './card-list.styles.css'
-import { Card } from '../card/card.component'
-
-export const CardList = (props) =>{
-    return <div className = 'card-list' >      
-        {props.monsters.map(monsters => (
-        <Card key={monsters.id}  monsters={monsters}/>
-      ))} </div>
-}
